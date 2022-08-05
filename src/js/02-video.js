2fb329e3f94faa8e03e4da736be82f81582a4b6b
MashaSam <samokhvalova.masha20@gmail.com>
2022-08-05 19:43:22 +0300

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on('play',onPlay)
+  function onPlay() {
+    console.log('played the video!');
+    
+};
+  
+player.on('timeupdate', throttle(onTimeupdate , 1000));
+  function onTimeupdate(data) {
+    
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data.seconds);
+    
+    
+};
+
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+ player.setCurrentTime(Number(currentTime));
+  // localStorage.setItem('videoplayer-current-time', timeupdateJSON);
+        
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+  
+
+  // Добавьте прослушиватель событий для указанного события.
+  //  Вызовет обратный вызов с одним параметром, dataкоторый содержит данные для этого события.Подробности смотрите ниже.
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput(event) {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const infoObj = { email, message };
+
+  const infoJSON = JSON.stringify(infoObj);
+
+    localStorage.setItem('feedback-form-state', infoJSON);
+    console.log(infoJSON);
+}
+const formInfo = localStorage.getItem('feedback-form-state');
+if (formInfo) {
+  const newObjInfo = JSON.parse(formInfo);
+  form.elements.email.value = newObjInfo.email;
+  form.elements.message.value = newObjInfo.message;
+}
+
+form.addEventListener('submit', onSubmitInput);
+function onSubmitInput(event) {
+  event.preventDefault();
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  // if ( email && message) { другой способ if(продвинутый)
+    if (email === '' || message === '') {
+        alert('Заполни все поля!')
+        return;
+    }
+    localStorage.removeItem('feedback-form-state');
+
+    console.log({ email, message });
+    form.reset();
+  
+}
